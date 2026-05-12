5418730c10be49d6e1b4e609213d941f36c2b174
IbraheemA05 <binfin.binfin@gmail.com>
2026-05-12 14:14:02 +0100

    Oppoa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../invoice-system.jsx';
+import App from './invoice-system.jsx';
 
 // Storage polyfill for window.storage API
 if (!window.storage) {
